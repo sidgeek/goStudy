0102207069633927d0d3907019ad331863aa76e6
sidgeek <sid.shi@nirvana-info.com>
2019-09-30 20:53:11 +0800

    add api test

---
+++ videoServer/api/dbops/api_test.go
@@ -0,0 +1,54 @@
+package dbops
+
+import "testing"
+
+func clearTables() {
+	dbConn.Exec("truncate users")
+	dbConn.Exec("truncate video_info")
+	dbConn.Exec("truncate comments")
+	dbConn.Exec("truncate session")
+}
+
+func TestMain(m *testing.M) {
+	clearTables()
+	m.Run()
+	clearTables()
+}
+
+func TestUserWorkFlow(t *testing.T) {
+	t.Run("Add", testAddUser)
+	t.Run("Get", testGetUser)
+	t.Run("Delete", testDeleteUser)
+	t.Run("Reget", testRegetUser)
+}
+
+func testAddUser(t *testing.T) {
+	err := AddUserCredential("aveb", "123")
+	if err != nil {
+		t.Errorf("Error of AddUser: %v", err)
+	}
+}
+
+func testGetUser(t *testing.T) {
+	pwd, err := GetUserCredential("aveb")
+	if pwd != "123" || err != nil {
+		t.Errorf("Error of GetUser")
+	}
+}
+
+func testDeleteUser(t *testing.T) {
+	err := DeleteUser("aveb", "123")
+	if err != nil {
+		t.Errorf("Error of DeleteUser: %v", err)
+	}
+}
+
+func testRegetUser(t *testing.T) {
+	pwd, err := GetUserCredential("aveb")
+	if err != nil {
+		t.Errorf("Error of ReGetUser: %v", err)
+	}
+	if pwd != "" {
+		t.Errorf("Delete user test failde")
+	}
+}
